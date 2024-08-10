10fb73ef85d4c7d33fa70aac793bb12e77c4bacb
Wonoo Choo <choo.wonoo.1@gmail.com>
2024-08-10 12:42:26 -0500

    print time and header

--- src/mpc/gp_mpc_node.cpp
+++ src/mpc/gp_mpc_node.cpp
@@ -163,8 +163,8 @@ void Node::referenceCallback(const gp_rhce::ReferenceTrajectory::ConstPtr& msg) 
 }
 
 void Node::stateEstCallback(const nav_msgs::Odometry::ConstPtr& msg) {
-    // double time = ros::Time::now().toSec();
-    // std::cout << time << std::endl;
+    double time = ros::Time::now().toSec();
+    std::cout << time << " " << msg->header.seq << std::endl;
     std::vector<double> p = {msg->pose.pose.position.x,
                              msg->pose.pose.position.y,
                              msg->pose.pose.position.z}; 
